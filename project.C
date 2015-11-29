77e8c1655bf65cfb7496d01ddd21e952f4a509e1
Cockie <cockie.vdb@gmail.com>
2015-11-29 23:33:23 +0100

    dinges

--- project.C
+++ project.C
@@ -29,26 +29,58 @@ void project(){ // function with the same name as the .C file is automatically e
     //gDirectory->ls();
     //TH1D* histo = (TH1D*)gDirectory->Get("histoGluon_0");
 
-    //TCanvas *c1 = new TCanvas("c1","The Uniform random example");
+    TCanvas *c1 = new TCanvas("c1","The Uniform random example");
+    TH1D *histoZ = new TH1D("Z","histogram of uniform distribution",200,100,1000);
+    TH1D *histoWp = new TH1D("W+","histogram of uniform distribution",200,100,1000);
+    TH1D *histoWm = new TH1D("W-","histogram of uniform distribution",200,100,1000);
     /* Step:
      * 1. Create 2 partons with 2 x
      * 2. Let partons create W/Z boson somehow
      * 3. Let W/Z boson decay
      */
     double fmax=get_max();
-    //step 1: generate 2 x'es
-    Parton part1=make_parton(fmax);
-    Parton part2=make_parton(fmax);
-    cout<<"part1 "<<part1._type<<" "<<part1._x<<endl;
-    cout<<"part2 "<<part2._type<<" "<<part2._x<<endl;
-    TLorentzVector vec1;
-    vec1.SetPx(q*part1._x);
-    vec1.SetE(q*part1._x);
-    TLorentzVector vec2;
-    vec2.SetPx(-q*part2._x);
-    vec2.SetE(q*part2._x);
-    TLorentzVector vecW=vec1+vec2;
-    cout<<vecW.Px()<<" "<<vecW.E()<<endl;
+    for(q=100;q<=1000;++q){
+        for(int n=0;n<100000;++n){
+            //step 1: generate 2 x'es
+            Parton part1=make_parton(fmax);
+            Parton part2=make_parton(fmax);
+            //cout<<"part1 "<<part1._type<<" "<<part1._x<<endl;
+            //cout<<"part2 "<<part2._type<<" "<<part2._x<<endl;
+            TLorentzVector vec1;
+            vec1.SetPx(q*part1._x);
+            vec1.SetE(q*part1._x);
+            TLorentzVector vec2;
+            vec2.SetPx(-q*part2._x);
+            vec2.SetE(q*part2._x);
+            TLorentzVector vecW=vec1+vec2;
+            //cout<<vecW.Px()<<" "<<vecW.E()<<endl;
+            if (vecW.E()>=80.385){
+                //correct types to make a Z
+                if((part1._type=="Up" && part2._type=="AUp")||(part1._type=="AUp" && part2._type=="Up")||(part1._type=="Down" && part2._type=="ADown")||(part1._type=="ADown" && part2._type=="Down")){
+                    //enough mass to make a Z
+                    if (vecW.E()>=91.1876){
+                        //make Z
+                        histoZ->Fill(q);
+                        cout<<"Z "<<endl;
+                    }
+                    //correct types for W+
+                }else if((part1._type=="Up" && part2._type=="ADown")||(part1._type=="ADown" && part2._type=="Up")){
+                    if (vecW.E()>=80.385){
+                        //make W+
+                        histoWp->Fill(q);
+                        cout<<"Wp "<<endl;
+                    }
+                }else if((part1._type=="AUp" && part2._type=="Down")||(part1._type=="Down" && part2._type=="AUp")){
+                    if (vecW.E()>=80.385){
+                        //make W-
+                        histoWm->Fill(q);
+                        cout<<"Wm "<<endl;
+                    }
+                }
+            }
+        }
+    }
+
 
 
     //if (part1._type ==& part2)
@@ -64,11 +96,11 @@ void project(){ // function with the same name as the .C file is automatically e
             }
         }
 
-    }
+    }*/
     c1->cd();//make sure you plot on the right canvas
-    histo->Draw();
-    histo->SetMinimum(0);
-    c1->Update();*/
+    histoZ->Draw();
+    histoZ->SetMinimum(0);
+    c1->Update();
 
 }
 //cross section
