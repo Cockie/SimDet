d936a33683eab34ac2fd544525bf58bfd260f3ed
Cockie <cockie.vdb@gmail.com>
2015-12-13 17:18:18 +0100

    blaaaaaaa

--- project.C
+++ project.C
@@ -31,15 +31,20 @@ void project(){ // function with the same name as the .C file is automatically e
     //histoGluon->FitSlicesX(0, ybin, ybin,0);
     //gDirectory->ls();
     //TH1D* histo = (TH1D*)gDirectory->Get("histoGluon_0");
-
-    TCanvas *c1 = new TCanvas("c1","electron");
-    TCanvas *c2 = new TCanvas("c2","muon");
-    TH1D *histoZ = new TH1D("Z","electron",30,-3,3);
-    TH1D *histoZb = new TH1D("Z","muon",30,-3,3);
-    TH1D *histoWp = new TH1D("W+","electron",30,-3,3);
-    TH1D *histoWpb = new TH1D("W+","muon",30,-3,3);
-    TH1D *histoWm = new TH1D("W-","electron",30,-3,3);
-    TH1D *histoWmb = new TH1D("W-","muon",30,-3,3);
+    TCanvas *c1 = new TCanvas("c1","Asymetry electron");
+    TCanvas *c2 = new TCanvas("c2","Asymetry muon");
+    TCanvas *cz = new TCanvas("cz","Z electron");
+    TH1D *histoZ = new TH1D("Ze","Z electron",30,-10,10);
+    TCanvas *czb = new TCanvas("czb","Z muon");
+    TH1D *histoZb = new TH1D("Zm","Z muon",30,-10,10);
+    TCanvas *cwp = new TCanvas("cwp","Wp electron");
+    TH1D *histoWp = new TH1D("W+e","Wp electron",30,-10,10);
+    TCanvas *cwpb = new TCanvas("cwpb","Wp muon");
+    TH1D *histoWpb = new TH1D("W+m","Wp muon",30,-10,10);
+    TCanvas *cwm = new TCanvas("cwm","Wm electron");
+    TH1D *histoWm = new TH1D("W-e","Wm electron",30,-10,10);
+    TCanvas *cwmb = new TCanvas("cwmb","Wm muon");
+    TH1D *histoWmb = new TH1D("W-m","Wm muon",30,-10,10);
     /* Step:
      * 1. Create 2 partons with 2 x
      * 2. Let partons create W/Z boson somehow
@@ -51,7 +56,7 @@ void project(){ // function with the same name as the .C file is automatically e
     int wm=0;
     int z=0;
     for(q=90;q<=90;++q){
-        for(int n=0;n<100000;++n){
+        for(int n=0;n<500000;++n){
             if(n%1000==0){
                 cout<<"n="<<n<<endl;}
             //step 1: generate 2 x'es
@@ -59,19 +64,17 @@ void project(){ // function with the same name as the .C file is automatically e
             Parton part2=make_parton(fmax);
             //cout<<"part1 "<<part1._type<<" "<<part1._x<<endl;
             //cout<<"part2 "<<part2._type<<" "<<part2._x<<endl;
-            TLorentzVector vec1;
-            vec1.SetPx(Etot*part1._x);
-            vec1.SetE( Etot*part1._x );
-            TLorentzVector vec2;
-            vec2.SetPx(-Etot*part2._x);
-            vec2.SetE( Etot*part2._x );
-            TLorentzVector vecW=vec1+vec2;
+            //TLorentzVector vec1(0, 0,Etot*part1._x,Etot*part1._x );
+            //TLorentzVector vec2(0, 0,-Etot*part2._x,Etot*part2._x );
+            //TLorentzVector vecW=vec1+vec2;
+            TLorentzVector vecW(0,0,Etot*part1._x-Etot*part2._x,Etot*part1._x+Etot*part2._x);
             //cout<<vecW.Px()<<" "<<vecW.E()<<endl;
             if (vecW.E()>=75){
+                double M=sqrt(vecW.E()*vecW.E()-vecW.P()*vecW.P());
                 //correct types to make a Z
                 if((part1._type=="Up" && part2._type=="AUp")||(part1._type=="AUp" && part2._type=="Up")||(part1._type=="Down" && part2._type=="ADown")||(part1._type=="ADown" && part2._type=="Down")){
                     //enough mass to make a Z
-                    if (vecW.E()>=91.1876){
+                    if (M>=91.1876){
                         //make Z
                         ++z;
                         //get the boost vector
@@ -91,8 +94,8 @@ void project(){ // function with the same name as the .C file is automatically e
 
                         veca.Boost(boost);
                         vecb.Boost(boost);
-                        histoZ->Fill(veca.PseudoRapidity());
-                        histoZ->Fill(vecb.PseudoRapidity());
+                        histoZ->Fill(veca.Rapidity());
+                        histoZ->Fill(vecb.Rapidity());
                         //muon
                         x=acos(2*rng.Uniform(0,1)-1);
                         P=sqrt(E*E-mumass*mumass);
@@ -101,14 +104,14 @@ void project(){ // function with the same name as the .C file is automatically e
 
                         veca.Boost(boost);
                         vecb.Boost(boost);
-                        histoZb->Fill(veca.PseudoRapidity());
-                        histoZb->Fill(vecb.PseudoRapidity());
+                        histoZb->Fill(veca.Rapidity());
+                        histoZb->Fill(vecb.Rapidity());
 
 
                     }
                     //correct types for W+
                 }else if((part1._type=="Up" && part2._type=="ADown")||(part1._type=="ADown" && part2._type=="Up")){
-                    if (vecW.E()>=80.385){
+                    if (M>=80.385){
                         //make W+
                         ++wp;
                         TVector3 boost=vecW.BoostVector();
@@ -121,11 +124,9 @@ void project(){ // function with the same name as the .C file is automatically e
                         double x=acos(2*rng.Uniform(0,1)-1);
                         double E=(vecW.E()*vecW.E()+emass*emass)/(2*vecW.E());
                         double P=sqrt(E*E-emass*emass);
-                        //double P=sqrt(E*E-0.1*0.1);
                         TLorentzVector veca(0, P*sin(x),P*cos(x),E );
-
                         veca.Boost(boost);;
-                        histoWp->Fill(veca.PseudoRapidity());
+                        histoWp->Fill(veca.Rapidity());
                         /*//second electron
                         x=acos(2*rng.Uniform(0,1)-1);
                         veca=TLorentzVector (0, P*sin(x),P*cos(x),E );
@@ -137,12 +138,11 @@ void project(){ // function with the same name as the .C file is automatically e
                         E=(vecW.E()*vecW.E()+mumass*mumass)/(2*vecW.E());
                         P=sqrt(E*E-mumass*mumass);
                         veca=TLorentzVector(0, P*sin(x),P*cos(x),E );
-
                         veca.Boost(boost);
-                        histoWpb->Fill(veca.PseudoRapidity());
+                        histoWpb->Fill(veca.Rapidity());
                     }
                 }else if((part1._type=="AUp" && part2._type=="Down")||(part1._type=="Down" && part2._type=="AUp")){
-                    if (vecW.E()>=80.385){
+                    if (M>=80.385){
                         //make W-
                         ++wm;
                         TVector3 boost=vecW.BoostVector();
@@ -157,7 +157,7 @@ void project(){ // function with the same name as the .C file is automatically e
                         double P=sqrt(E*E-emass*emass);
                         TLorentzVector veca(0, P*sin(x),P*cos(x),E );
                         veca.Boost(boost);;
-                        histoWm->Fill(veca.PseudoRapidity());
+                        histoWm->Fill(veca.Rapidity());
                         /*//second
                         x=acos(2*rng.Uniform(0,1)-1);
                         veca=TLorentzVector(0, P*sin(x),P*cos(x),E );
@@ -170,7 +170,7 @@ void project(){ // function with the same name as the .C file is automatically e
                         veca=TLorentzVector(0, P*sin(x),P*cos(x),E );
 
                         veca.Boost(boost);
-                        histoWmb->Fill(veca.PseudoRapidity());
+                        histoWmb->Fill(veca.Rapidity());
                     }
                 }
             }
@@ -180,11 +180,11 @@ void project(){ // function with the same name as the .C file is automatically e
 
 
     //if (part1._type ==& part2)
-    /*TH1D *histo = new TH1D("Up_test","histogram of uniform distribution",200,0,1);
+    /*TH1D *histo = new TH1D("Up_test","histogram of uniform distribution",50,0,1);
     int prod=0;
-    while (prod<5000){
+    while (prod<10000){
         Parton part=make_parton(fmax);
-        if (part._type=="ADown"){
+        if (part._type=="Down"){
             histo->Fill(part._x);
             prod++;
             if(prod%100==0){
@@ -192,32 +192,57 @@ void project(){ // function with the same name as the .C file is automatically e
             }
         }
 
-    }*/
-    /*c1->cd();//make sure you plot on the right canvas
+    }
+    c1->cd();//make sure you plot on the right canvas
+    histo->Draw();
+    histo->SetMinimum(0);
+    c1->Update();
+    */
+    cwm->cd();//make sure you plot on the right canvas
     histoWm->Draw();
     histoWm->SetMinimum(0);
-    c1->Update();
+    cwm->Update();
 
-    c2->cd();//make sure you plot on the right canvas
+    cwmb->cd();//make sure you plot on the right canvas
     histoWmb->Draw();
     histoWmb->SetMinimum(0);
-    c2->Update();*/
+    cwmb->Update();
+
+    cz->cd();//make sure you plot on the right canvas
+    histoZ->Draw();
+    histoZ->SetMinimum(0);
+    cz->Update();
+
+    czb->cd();//make sure you plot on the right canvas
+    histoZb->Draw();
+    histoZb->SetMinimum(0);
+    czb->Update();
+
+    cwpb->cd();//make sure you plot on the right canvas
+    histoWpb->Draw();
+    histoWpb->SetMinimum(0);
+    cwpb->Update();
+
+    cwp->cd();//make sure you plot on the right canvas
+    histoWp->Draw();
+    histoWp->SetMinimum(0);
+    cwp->Update();
     cout<<"Z="<<z<<" Wmin="<<wm<<" Wplus="<<wp<<endl;
-    TH1D *ass = new TH1D("W-","electron",30,-3,3);
-    TH1D *ass_sub = new TH1D("W-","electron",30,-3,3);
-    TH1D *ass_sum = new TH1D("W-","electron",30,-3,3);
-    ass_sub->Add(histoWm, histoWp,-1,2);
-    ass_sum->Add(histoWm, histoWp,1,2);
+    TH1D *ass = new TH1D("ass","electron",30,-10,10);
+    TH1D *ass_sub = new TH1D("ass_sub","electron",30,-10,10);
+    TH1D *ass_sum = new TH1D("ass_sum","electron",30,-10,10);
+    ass_sub->Add(histoWm, histoWp,-1,1);
+    ass_sum->Add(histoWm, histoWp,1,1);
     ass->Divide(ass_sub, ass_sum);
     c1->cd();//make sure you plot on the right canvas
     ass->Draw();
     ass->SetMinimum(0);
     c1->Update();
-    TH1D *assb = new TH1D("W-","muon",30,-3,3);
-    TH1D *assb_sub = new TH1D("W-","muon",30,-3,3);
-    TH1D *assb_sum = new TH1D("W-","muon",30,-3,3);
-    assb_sub->Add(histoWmb, histoWpb,-1,2);
-    assb_sum->Add(histoWmb, histoWpb,1,2);
+    TH1D *assb = new TH1D("assb","muon",30,-10,10);
+    TH1D *assb_sub = new TH1D("assb_sub","muon",30,-10,10);
+    TH1D *assb_sum = new TH1D("assb_sum","muon",30,-10,10);
+    assb_sub->Add(histoWmb, histoWpb,-1,1);
+    assb_sum->Add(histoWmb, histoWpb,1,1);
     assb->Divide(assb_sub, assb_sum);
     c2->cd();//make sure you plot on the right canvas
     assb->Draw();
